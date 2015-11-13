992d81b1963a4cff6311731dbf3ec46297c5aa00
dasu1 <nite1270@gmail.com>
2015-11-13 11:30:35 +0800

    game

---
+++ game/js/index.js
@@ -0,0 +1,86 @@
+function game() {
+  alert("HEY, wake up!");
+  alert("Get up it's your big day!");
+  var memory = prompt("You do wemember what's happening today wight?");
+
+  if (memory === "yeah") {
+    alert("YEAH AS IF!");
+    alert("Well then  if you no then WHY AWEN'T YOU WEADY YET!?");
+    alert("*gets ready*")
+    alert("later at the arena gates...")
+
+    var age = prompt("Age, please?");
+
+    if (age < 12) {
+      alert("Sorry buddy, but you're not eligible");
+    } else {
+      alert("Alright, your're good to go...");
+      var clan = prompt("Llama or Alpaca?")
+      if (clan === "llama") {
+        alert("Ah, welcome back brother!");
+      } else {
+        alert("WHAT ARE YOU DOING HERE, GET OUT!");
+      }
+    }
+    alert("The gates to the city close. You could even hear the Dragon whispering to you. You're suddenly trapped in an overcast of Darkness....The grip on your blade gets tighter...")
+    alert("*tap tap*")
+    alert("SOMETHING COLLAPSED")
+    alert("You hear a smoky laugh")
+    alert("A BURST OF FIRE")
+
+    var away = prompt("Move or Stay")
+    if (away === "move") {
+      alert("You tripped! You seem to be trapped in some sort of cage...");
+    } else {
+      alert("A BURST OF FIRE COMES STRAIGHT AT YOU AND YOU BURN DOWN TO THE GROUND!");
+    }
+
+    alert("*drip, drip*")
+
+    var CENA = prompt("Ask who it is or stay still?")
+    if (CENA === "ask who it is") {
+      alert("MY NAME IS JOHN CENA!");
+    } else {
+      alert("A BURST OF FIRE COMES STRAIGHT AT YOU AND YOU BURN DOWN TO THE GROUND!");
+    }
+  } else {
+    alert("shouldv'e guessed, well today's Dwagon slaying day. When all the big boys and girls in the village head into an arena to fight a big SKAWY dwagun.");
+    alert("*gets ready*")
+    alert("later at the arena gates...")
+
+    var age = prompt("How old are you again?");
+
+    if (age < 12) {
+      alert("Sorry buddy, but you're not eligible");
+    } else {
+      alert("Alright, your're good to go...");
+      var clan = prompt("Llama or Alpaca?")
+      if (clan === "llama") {
+        alert("Ah, welcome back brother!");
+      } else {
+        alert("WHAT ARE YOU DOING HERE, GET OUT!");
+      }
+    }
+    alert("The gates to the city close. You could even hear the Dragon whispering to you. You're suddenly trapped in an overcast of Darkness....The grip on your blade gets tighter...")
+    alert("*tap tap*")
+    alert("plain darkness...")
+    alert("You hear a smoky laugh")
+    alert("*ssss*")
+
+    var away = prompt("Move or Stay")
+    if (away === "move") {
+      alert("You tripped! You seem to be trapped in some sort of cage...");
+    } else {
+      alert("A BURST OF FIRE COMES STRAIGHT AT YOU AND YOU BURN DOWN TO THE GROUND!");
+    }
+
+    alert("*drip, drip*")
+
+    var CENA = prompt("Ask who it is or stay still?")
+    if (CENA === "ask who it is") {
+      alert("MY NAME IS JOHN CENA!");
+    } else {
+      alert("A BURST OF FIRE COMES STRAIGHT AT YOU AND YOU BURN DOWN TO THE GROUND!");
+    }
+  }
+}
